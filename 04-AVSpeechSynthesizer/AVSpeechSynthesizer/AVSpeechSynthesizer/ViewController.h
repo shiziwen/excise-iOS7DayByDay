ca47e1f73e40169ce0958dd0a7a6625a71829d6b
shiziwen <shiziwen@gmail.com>
2016-01-30 05:42:32 +0800

    init for AVSpeechSynthesizer

---
+++ 04-AVSpeechSynthesizer/AVSpeechSynthesizer/AVSpeechSynthesizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVSpeechSynthesizer
+//
+//  Created by mac on 16/1/30.
+//  Copyright © 2016年 shiziwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
