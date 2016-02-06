d4ab1d422a1a5ad3e83ce1bc545ea3455a6ef085
shiziwen <shiziwen@gmail.com>
2016-02-06 23:58:27 +0800

    added some ui

--- 06-TinyColor/ColorChanger/ColorChanger/ViewController.m
+++ 06-TinyColor/ColorChanger/ColorChanger/ViewController.m
@@ -24,4 +24,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)changeColorHandler:(id)sender {
+    // Generate a random color
+    CGFloat hue = ( arc4random() % 256 / 256.0 );
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;
+    UIColor *color = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+    self.view.tintColor = color;
+    [self updateProgressViewTint];
+}
+
+- (void)updateProgressViewTint
+{
+    self.progressView.progressTintColor = self.view.tintColor;
+}
+
 @end
