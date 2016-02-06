560f5ab3bea932e26db54bab93c604030407fdec
shiziwen <shiziwen@gmail.com>
2016-02-06 23:21:17 +0800

    init 07-tinycolor

---
+++ 06-TinyColor/ColorChanger/ColorChanger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorChanger
+//
+//  Created by mac on 16/2/6.
+//  Copyright © 2016年 shiziwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 06-TinyColor/ColorChanger/ColorChanger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorChanger
+//
+//  Created by mac on 16/2/6.
+//  Copyright © 2016年 shiziwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
